e9208910417d332f2f4fe97e5a3698a8a80d071c
shahirahsabran <54113776+shahirahsabran@users.noreply.github.com>
2022-12-12 16:55:17 +0800

    Add files via upload

---
+++ main/java/com/example/zakatfinal/AboutPage.java
@@ -0,0 +1,25 @@
+package com.example.zakatfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class AboutPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_page);
+
+        activityHyperlink();
+    }
+
+    private void activityHyperlink () {
+        TextView textViewLink = findViewById(R.id.textView9);
+        textViewLink.setMovementMethod(LinkMovementMethod.getInstance());
+        textViewLink.setLinkTextColor(Color.BLUE);
+    }
+}
